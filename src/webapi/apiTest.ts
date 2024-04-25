544226617972bac53b4421f19218eb2e72faa190
takaka-t <86246662+takaka-t@users.noreply.github.com>
2024-04-25 19:02:51 +0900

    webapi呼び出し処理のTest修正

--- src/webapi/apiTest.ts
+++ src/webapi/apiTest.ts
@@ -5,16 +5,10 @@ import { http } from "@/commons/http";
  */
 export namespace ApiTest {
   /**
-   * get-test APIのレスポンス
-   */
-  interface GetTestResponse {
-    result: string;
-  }
-  /**
    * get-test テスト用API
    */
-  export const getTest = async (): Promise<GetTestResponse> => {
-    const response = await http.get<GetTestResponse>("test/get-test");
-    return response.data;
+  export const getTest = async (): Promise<{ result: string }> => {
+    const response = await http.get("test/get-test");
+    return { result: String(response.data.result) };
   };
 }
